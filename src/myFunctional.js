b1411565465a3c7f66a94142259c23d129861710
praveenkumarpke <praveenpke@gmail.com>
2022-05-09 16:41:05 +0530

    created functional components

---
+++ src/myFunctional.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Myfunction() {
+    return (
+        <div>
+            <h1>brand: "Ford",</h1>
+            <h2>model: "Mustang",</h2>
+            <h3> color: "red",</h3>
+            <h4> year: 1964</h4>
+        </div>
+    )
+}
+
+export default Myfunction;
